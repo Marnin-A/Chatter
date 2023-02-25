ccc3c0cef5b442a08779a61edd2bda10cc8d6a68
Marnin Audu <msmaudu@gmail.com>
2023-02-25 22:48:48 +0100

    Create user models file

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL);
 
 app.get("/test", (req, res) => {
   res.json("test ok");
 });
-
+app.post("/register", (req, res) => {});
 app.listen(4000);
